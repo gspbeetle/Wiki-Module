b1832945d24e2822091f225f7774ee7b6a73e8cf
gspbeetle <33320637+gspbeetle@users.noreply.github.com>
2025-07-26 10:01:27 +0800

    Update PrismCardCode.lua
    Add Chihiya, Miki

--- src/PrismCardCode.lua
+++ src/PrismCardCode.lua
@@ -65,6 +65,8 @@ p.character_code = {
     [89] = "Haruka Amami",
     [88] = "Makoto Kikuchi",
     [87] = "Takane Shijou",
+    [86] = "Chihaya Kisaragi",
+    [85] = "Miki Hoshii",
 }
 
 ---@param s string string to trim
